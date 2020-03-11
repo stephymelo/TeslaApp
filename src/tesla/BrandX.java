df7268d35c35a2ba85045b3a8c95d9d4e0ca05d7
stephym <60659457+stephymelo@users.noreply.github.com>
2020-03-10 23:54:16 -0500

    brand x stuff

--- src/tesla/BrandX.java
+++ src/tesla/BrandX.java
@@ -3,10 +3,40 @@ package tesla;
 import processing.core.PApplet;
 
 public class BrandX extends Carro {
+	private int display;
+	private int batteryRange;
+	private int cargo;
 
 	public BrandX(PApplet app) {
 		super(app);
+		this.batteryRange=batteryRange;
+		this.display=display;
+		this.cargo=cargo;
 	
+	}
+
+	public int getDisplay() {
+		return display;
+	}
+
+	public void setDisplay(int display) {
+		this.display = display;
+	}
+
+	public int getBatteryRange() {
+		return batteryRange;
+	}
+
+	public void setBatteryRange(int batteryRange) {
+		this.batteryRange = batteryRange;
+	}
+
+	public int getCargo() {
+		return cargo;
+	}
+
+	public void setCargo(int cargo) {
+		this.cargo = cargo;
 	}
 
 }
